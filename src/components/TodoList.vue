767f5db48cec87d76b453559d35ecd39daccdbe9
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2018-06-17 23:28:35 -0300

    ajustando animação dos botões

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
   div(:class="{ completed: todo.done }")
-    input(type="checkbox" :checked="todo.done" @change="onCompleteTask(todo)" :value="todo.done")
-    h4.card-list__text.card-list__text--border  {{ todo.text }}
+    input(type="checkbox" :checked="todo.done" @change="onCompleteTask(todo)" class="custom-check" id="type-check" :value="todo.done")
+    label(for="type-check")
+    h4(class="card-list__text card-list__text--border")
+      span {{ todo.text }}
 </template>
 
 <script lang="ts">
@@ -25,28 +27,71 @@ export default class TodoList extends Vue {
   .completed {
     .card-list__text {
       color: lighten($shadow-default, 65%);
-      transition: all 0.4s ease-in-out;
+      transition: all 0.3s ease-in-out;
     }
     .card-list__text--border::before {
-      top: 50%;
+      transform: translateY(-12px);
     }
   }
   .card-list__text {
-    font-size: 1.6em;
+    font-size: 1.4em;
+    font-weight: 100;
     margin-left: 0.5em;
     flex: 1 1 auto;
     color: $shadow-default;
     padding-bottom: 0.2em;
     &.card-list__text--border {
       position: relative;
+      transition: all 0.4s ease-in-out;
       &::before {
         content: '';
+        transition: all 0.3s ease-in-out;
         position: absolute;
         bottom: 0;
         background-color: lighten($shadow-default, 65%);
         left: 0;
         width: 100%;
         height: 1px;
+      }
+    }
+  }
+  .custom-check {
+    display: none;
+    &:checked {
+      & + label {
+        border-color: none;
+      }
+      & + label::before,
+      & + label::after {
+        opacity: 1;
+      }
+    }
+    & + label {
+      cursor: pointer;
+      position: relative;
+      border-radius: 100%;
+      width: 30px;
+      height: 30px;
+      box-shadow: 0 0 0 1px #ccc;
+      &::before,
+      &::after {
+        content: '';
+        position: absolute;
+        opacity: 0;
+        transition: all 0.3s ease;
+        left: 50%;
+        top: 50%;
+        background-color: #333;
+      }
+      &::before {
+        width: 6px;
+        height: 2px;
+        transform: translate(-114%, -50%) rotate(47deg);
+      }
+      &::after {
+        width: 12px;
+        height: 2px;
+        transform: translate(-29%, -100%) rotate(135deg);
       }
     }
   }
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,4 +1,13 @@
 <template lang="pug">
-  .about
+  div(class="about")
     h1 This is an about page
 </template>
+<style lang="scss" scoped>
+  .about {
+    margin: 0 auto;
+    max-width: 800px;
+    background-color: #fff;
+    padding: 3em;
+  }
+</style>
+
